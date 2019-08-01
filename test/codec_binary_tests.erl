15e154b3c2f4fa844c25a7a6c30fee50de64ac82
Sascha Kattelmann <sascha.kattelmann@stritzinger.com>
2019-08-01 10:13:46 +0200

    Add unit test for create session response and align testing method

--- test/codec_binary_tests.erl
+++ test/codec_binary_tests.erl
@@ -10,8 +10,8 @@ t_test_() ->
      [
       fun open_secure_channel_request/0,
       fun open_secure_channel_response/0,
-      fun create_session_request/0
-      %fun create_session_response/0,
+      fun create_session_request/0,
+      fun create_session_response/0
       %fun activate_session_request/0,
       %fun activate_session_response/0,
       %fun read_request/0,
@@ -26,13 +26,30 @@ setup() ->
 cleanup(_) ->
     ok = application:stop(opcua).
 
-%% some little helper
-assert_codec(NodeId, ToBeEncoded, Encoded) ->
-    {Bin, _} = opcua_codec_binary:encode(NodeId, ToBeEncoded),
-    ?assertEqual(Encoded, string:to_lower(opcua_util:bin_to_hex(Bin))),
-    {Decoded, EmptyBin} = opcua_codec_binary:decode(NodeId, Bin),
+%% This little helper does the major testing:
+%%
+%% 1. encode some data according to a NodeId
+%% 2. decode the result again and check if it
+%%    matches the original data
+%% 3. decode another example from the python
+%%    implementation which encoded the same
+%%    data and check if the data matches
+%%    again (hex dumps are from wireshark)
+%%
+%% Why not compare the encoded binaries
+%% directly? Because NodeIds can be encoded
+%% in different ways and hence you can get
+%% binaries holding the same data even though
+%% the binaries are not the same!
+assert_codec(NodeId, ToBeEncoded, EncodedComp) ->
+    {Encoded, _} = opcua_codec_binary:encode(NodeId, ToBeEncoded),
+    {Decoded, EmptyBin} = opcua_codec_binary:decode(NodeId, Encoded),
     ?assertEqual(<<>>, EmptyBin),
-    ?assertEqual(ToBeEncoded, Decoded).
+    ?assertEqual(ToBeEncoded, Decoded),
+    BinEncodedComp = opcua_util:hex_to_bin(EncodedComp),
+    {DecodedComp, EmptyBin1} = opcua_codec_binary:decode(NodeId, BinEncodedComp),
+    ?assertEqual(<<>>, EmptyBin1),
+    ?assertEqual(ToBeEncoded, DecodedComp).
 
 open_secure_channel_request() ->
     NodeId = #node_id{value = 444},
@@ -124,4 +141,99 @@ create_session_request() ->
               "1af025dabacbfdcfaa4891d0cd9fe09a3addb2e0"
               "94db4048dcffffffff0000000040774b41000000"
               "00",
+    assert_codec(NodeId, ToBeEncoded, Encoded).
+
+create_session_response() ->
+    NodeId = #node_id{value = 462},
+    ToBeEncoded = #{
+        authentication_token => #node_id{value = 1001},
+        max_request_message_size => 65536,
+        response_header => #{
+            additional_header => #extension_object{},
+            request_handle => 2,
+            service_diagnostics => #diagnostic_info{},
+            service_result => 0,
+            string_table => [],
+            timestamp => 132061913263448480
+        },
+        revised_session_timeout => 3600000.0,
+        server_certificate => undefined,
+        server_endpoints => [#{
+            endpoint_url => <<"opc.tcp://127.0.0.1:4840/freeopcua/server/">>,
+            security_level => 0,
+            security_mode => none,
+            security_policy_uri => <<"http://opcfoundation.org/UA/SecurityPolicy#None">>,
+            server => #{
+                application_name => #localized_text{text = <<"FreeOpcUa Python Server">>},
+                application_type => client_and_server,
+                application_uri => <<"urn:freeopcua:python:server">>,
+                discovery_profile_uri => undefined,
+                discovery_urls => [<<"opc.tcp://0.0.0.0:4840/freeopcua/server/">>],
+                gateway_server_uri => undefined,
+                product_uri => <<"urn:freeopcua.github.io:python:server">>
+            },
+            server_certificate => undefined,
+            transport_profile_uri => <<"http://opcfoundation.org/UA-Profile/Transport/uatcp-uasc-uabinary">>,
+            user_identity_tokens => [
+                #{
+                    issued_token_type => undefined,
+                    issuer_endpoint_url => undefined,
+                    security_policy_uri => undefined,
+                    policy_id => <<"anonymous">>,
+                    token_type => anonymous
+                },
+                #{
+                    issued_token_type => undefined,
+                    issuer_endpoint_url => undefined,
+                    security_policy_uri => undefined,
+                    policy_id => <<"certificate_basic256sha256">>,
+                    token_type => certificate
+                },
+                #{
+                    issued_token_type => undefined,
+                    issuer_endpoint_url => undefined,
+                    security_policy_uri => undefined,
+                    policy_id => <<"username">>,
+                    token_type => user_name
+                }
+            ]
+        }],
+        server_nonce => opcua_util:hex_to_bin("68924a95b8434526a36cb9373085289748b9dd60fbdff38153339e7844ef8c14"),
+        server_signature => #{
+            algorithm => <<"http://www.w3.org/2000/09/xmldsig#rsa-sha1">>,
+            signature => <<>>
+        },
+        server_software_certificates => [],
+        session_id => #node_id{value = 11}
+    },
+    Encoded = "a0216d449c2dd501020000000000000000000000"
+              "000000000200000b000000020000e90300000000"
+              "000040774b412000000068924a95b8434526a36c"
+              "b9373085289748b9dd60fbdff38153339e7844ef"
+              "8c14ffffffff010000002a0000006f70632e7463"
+              "703a2f2f3132372e302e302e313a343834302f66"
+              "7265656f706375612f7365727665722f1b000000"
+              "75726e3a667265656f706375613a707974686f6e"
+              "3a7365727665722500000075726e3a667265656f"
+              "706375612e6769746875622e696f3a707974686f"
+              "6e3a7365727665720217000000467265654f7063"
+              "556120507974686f6e2053657276657202000000"
+              "ffffffffffffffff01000000280000006f70632e"
+              "7463703a2f2f302e302e302e303a343834302f66"
+              "7265656f706375612f7365727665722fffffffff"
+              "010000002f000000687474703a2f2f6f7063666f"
+              "756e646174696f6e2e6f72672f55412f53656375"
+              "72697479506f6c696379234e6f6e650300000009"
+              "000000616e6f6e796d6f757300000000ffffffff"
+              "ffffffffffffffff1a0000006365727469666963"
+              "6174655f62617369633235367368613235360200"
+              "0000ffffffffffffffffffffffff080000007573"
+              "65726e616d6501000000ffffffffffffffffffff"
+              "ffff41000000687474703a2f2f6f7063666f756e"
+              "646174696f6e2e6f72672f55412d50726f66696c"
+              "652f5472616e73706f72742f75617463702d7561"
+              "73632d756162696e61727900000000002a000000"
+              "687474703a2f2f7777772e77332e6f72672f3230"
+              "30302f30392f786d6c64736967237273612d7368"
+              "61310000000000000100",
     assert_codec(NodeId, ToBeEncoded, Encoded).
