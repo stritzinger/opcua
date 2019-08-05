3574e8ded59831eda218b7792a0e2cb47e41b0f5
Sascha Kattelmann <sascha.kattelmann@stritzinger.com>
2019-08-05 12:06:25 +0200

    Move common parsing utilities funcs into opcua_util.erl

--- src/opcua_codec_encodings.erl
+++ src/opcua_codec_encodings.erl
@@ -27,19 +27,19 @@ setup(File) ->
 %% INTERNAL
 
 parse({startElement, _, "UAObject", _, Attributes}, _Loc, _State) ->
-    NodeId = get_node_id("NodeId", Attributes),
-    case get_attr("BrowseName", Attributes) of
+    NodeId = opcua_util:get_node_id("NodeId", Attributes),
+    case opcua_util:get_attr("BrowseName", Attributes) of
         "Default Binary"    -> #{node_id => NodeId, type => binary};
         _                   -> undefined
     end;
 parse({startElement, _, "Reference", _, Attributes}, _Loc, EncodingTypeMap)
   when is_map(EncodingTypeMap) ->
-    case get_attr("ReferenceType", Attributes) of
+    case opcua_util:get_attr("ReferenceType", Attributes) of
         "HasEncoding"   -> {await_target_node_id, EncodingTypeMap};
         _               -> EncodingTypeMap
     end;
 parse({characters, Chars}, _Loc, {await_target_node_id, EncodingTypeMap}) ->
-    maps:put(target_node_id, parse_node_id(Chars), EncodingTypeMap);
+    maps:put(target_node_id, opcua_util:parse_node_id(Chars), EncodingTypeMap);
 parse({endElement, _, "UAObject", _}, _Loc, #{node_id := NodeId, target_node_id := TargetNodeId, type := Encoding}) ->
     store_encoding(NodeId, TargetNodeId, Encoding);
 parse(_Event, _Loc, State) ->
@@ -48,22 +48,3 @@ parse(_Event, _Loc, State) ->
 store_encoding(NodeId, TargetNodeId, Encoding) ->
     ets:insert(?DB_ENCODINGS, [{NodeId, {TargetNodeId, Encoding}},
                                {{TargetNodeId, Encoding}, NodeId}]).
-
-get_node_id(Key, Attributes) ->
-    case get_attr(Key, Attributes) of
-        undefined       -> undefined;
-        NodeIdString    -> parse_node_id(NodeIdString)
-    end.
-
-parse_node_id(String) ->
-    [_, String1] = string:split(String, "="),
-    opcua_codec:node_id(list_to_integer(String1)).
-
-get_attr(Key, Attributes) ->
-    get_attr(Key, Attributes, undefined).
-
-get_attr(Key, Attributes, Default) ->
-    case lists:keyfind(Key, 3, Attributes) of
-        false -> Default;
-        Value -> element(4, Value)
-    end.
