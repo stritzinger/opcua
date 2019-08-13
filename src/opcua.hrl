95dfbecbd22c7b3e60b874f9821c760fea1630fd
Adam Lindberg <hello@alind.io>
2019-08-13 14:06:43 +0200

    Remove unused header file

--- src/opcua.hrl
+++
@@ -1,8 +0,0 @@
-
-%%% TYPES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
--record(node_id, {
-    ns = default :: default | non_neg_integer(),
-    type = numeric :: numeric | string | guid | opaque,
-    value :: non_neg_integer() | binary()
-}).
