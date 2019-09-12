7a78c8a4087ba77e386cb89be33f9f9662016180
Sébastien Merle <s.merle@gmail.com>
2019-09-12 11:43:31 +0200

    Change client API to not return error tuples

--- test/opcua_system_tests.erl
+++ test/opcua_system_tests.erl
@@ -15,16 +15,16 @@ basic_client_server_connection_test_() ->
     setup_teardown([
         {"basic client/server connection", fun() ->
             application:ensure_all_started(opcua),
-            {ok, Client} = opcua_client:connect(<<"opc.tcp://127.0.0.1">>),
-            ?assertMatch({ok, #opcua_qualified_name{ns = 0, name = <<"Server">>}},
+            Client = opcua_client:connect(<<"opc.tcp://127.0.0.1">>),
+            ?assertMatch(#opcua_qualified_name{ns = 0, name = <<"Server">>},
                          opcua_client:read(Client, server, browse_name)),
             %FIXME: This should actually return 'object' for node class...
-            ?assertMatch({ok, [?NNID(?OBJ_SERVER), 1]},
+            ?assertMatch([?NNID(?OBJ_SERVER), 1],
                          opcua_client:read(Client, server, [node_id, node_class])),
-            {ok, Refs} = opcua_client:browse(Client, root, #{
-                            direction => forward,
-                            type => ?REF_ORGANIZES,
-                            include_subtypes => true}),
+            Refs = opcua_client:browse(Client, root, #{
+                direction => forward,
+                type => ?REF_ORGANIZES,
+                include_subtypes => true}),
             ?assertMatch([
                 #{browse_name := #opcua_qualified_name{name = <<"Objects">>}},
                 #{browse_name := #opcua_qualified_name{name = <<"Types">>}},
